c2f1ec558952a0b3b14e5797adeb9decb57ccfed
niko.brusselaers <niko.brusselaers@student.ehb.be>
2021-11-23 15:47:16 +0100

    insurance gitignore

---
+++ session 6/db/src/main/kotlin/main.kt
@@ -0,0 +1,20 @@
+import java.sql.*
+import java.util.*
+
+fun main() {
+    Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance()
+    // Prepare credentials
+    val credentials = credentials()
+    val connectionProps = Properties()
+    connectionProps["user"] = credentials.username
+    connectionProps["password"] = credentials.password
+
+// Create the connection: this will allow us to run queries on it later
+    val connection =  DriverManager.getConnection(
+        "jdbc:" + "mysql" + "://" +
+                "dt5.ehb.be" +
+                ":" + "3306" + "/" +
+                credentials.databaseName,
+        connectionProps)
+
+}
